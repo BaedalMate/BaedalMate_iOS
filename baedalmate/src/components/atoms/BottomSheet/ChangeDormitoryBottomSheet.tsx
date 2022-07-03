3a175772057b84d0854f9158be631798b69f7c53
byein <kyebin0825@gmail.com>
2022-07-04 02:26:20 +0900

    #5 - [chore] add modal bottom sheet onclick the TodayMenu dormitory

---
+++ baedalmate/src/components/atoms/BottomSheet/ChangeDormitoryBottomSheet.tsx
@@ -0,0 +1,119 @@
+import React, {useState} from 'react';
+import {FlatList, Modal, TouchableOpacity, View} from 'react-native';
+import {TextKRBold, TextKRReg} from 'themes/text';
+import {LINE_GRAY_COLOR, PRIMARY_COLOR, WHITE_COLOR} from 'themes/theme';
+
+export const DormitoryList = [
+  {id: 0, value: '성림학사'},
+  {id: 1, value: 'KB학사'},
+  {id: 2, value: '불암학사'},
+  {id: 3, value: '누리학사'},
+  {id: 4, value: '수림학사'},
+];
+const ChangeDormitory = userDormitory => {
+  const [modal, setModal] = useState(false);
+  const [selectedDormitory, setSelectedDormitory] = useState(userDormitory);
+  const handleModal = () => {
+    modal ? setModal(false) : setModal(true);
+  };
+
+  // const renderItem = ({item}) => {
+  //   return (
+  //     <TouchableOpacity
+  //       onPress={() => {
+  //         setSelectedAddress(item.value);
+  //       }}
+  //       style={{
+  //         height: 52,
+  //         width: '100%',
+  //         paddingHorizontal: 15,
+  //         justifyContent: 'center',
+  //         borderBottomColor: LINE_GRAY_COLOR,
+  //         borderBottomWidth: 1,
+  //       }}>
+  //       <TextKRReg
+  //         style={{
+  //           fontWeight: '400',
+  //           fontSize: 16,
+  //           lineHeight: 24,
+  //           color: selectedAddress === item.value ? PRIMARY_COLOR : '#666666',
+  //         }}>
+  //         {item.value}
+  //       </TextKRReg>
+  //     </TouchableOpacity>
+  //   );
+  // };
+
+  return (
+    <View>
+      {/* <Modal
+        transparent={true}
+        visible={modal}
+        animationType={'slide'}
+        onRequestClose={handleModal}>
+        <View
+          style={{
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0,0,0,0.45)',
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <View
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              width: '100%',
+              height: 383,
+              backgroundColor: WHITE_COLOR,
+              borderTopLeftRadius: 10,
+              borderTopRightRadius: 10,
+            }}>
+            <View
+              style={{
+                width: '100%',
+                height: 35,
+                justifyContent: 'center',
+                borderBottomWidth: 1,
+                borderBottomColor: LINE_GRAY_COLOR,
+              }}>
+              <TextKRReg
+                style={{
+                  textAlign: 'center',
+                  fontWeight: '400',
+                  fontSize: 14,
+                  lineHeight: 24,
+                }}>
+                배달거점
+              </TextKRReg>
+            </View>
+            <View>
+              <FlatList data={AddressData} renderItem={renderItem} />
+            </View>
+            <TouchableOpacity
+              onPress={handleModal}
+              style={{
+                width: '100%',
+                justifyContent: 'center',
+              }}>
+              <TextKRBold
+                style={{
+                  textAlign: 'center',
+                  paddingTop: 11,
+                  fontWeight: '400',
+                  fontSize: 16,
+                  lineHeight: 22,
+                }}>
+                확인
+              </TextKRBold>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal> */}
+    </View>
+  );
+};
+
+export default ChangeDormitory;
--- baedalmate/src/components/atoms/Main/UserInfoTitle.tsx
+++ baedalmate/src/components/atoms/Main/UserInfoTitle.tsx
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {StyleSheet, View, Text} from 'react-native';
-import {
-  ALARM_WHITE,
-  PRIMARY_COLOR,
-  SEARCH_WHITE,
-  WHITE_COLOR,
-} from 'themes/theme';
+import {View, Modal, FlatList} from 'react-native';
+import {LINE_GRAY_COLOR, PRIMARY_COLOR, WHITE_COLOR} from 'themes/theme';
 import {TextKRBold, TextKRReg} from 'themes/text';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import ChangeDormitory, {
+  DormitoryList,
+} from '../BottomSheet/ChangeDormitoryBottomSheet';
+
+const Item = ({title}) => null;
+export type UserAddressProps = {
+  onPress(): void;
+  text: string;
+};
+
 const UserInfoTitle = ({userName, userAddress}) => {
+  const [modal, setModal] = useState(false);
+  const [selectedAddress, setSelectedAddress] = useState(userAddress);
+  const handleModal = () => {
+    modal ? setModal(false) : setModal(true);
+  };
+  const renderItem = ({item}) => {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          setSelectedAddress(item.value);
+        }}
+        style={{
+          height: 52,
+          width: '100%',
+          paddingHorizontal: 15,
+          justifyContent: 'center',
+          borderBottomColor: LINE_GRAY_COLOR,
+          borderBottomWidth: 1,
+        }}>
+        <TextKRReg
+          style={{
+            fontWeight: '400',
+            fontSize: 16,
+            lineHeight: 24,
+            color: selectedAddress === item.value ? PRIMARY_COLOR : '#666666',
+          }}>
+          {item.value}
+        </TextKRReg>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <>
       <View
@@ -18,7 +56,7 @@ const UserInfoTitle = ({userName, userAddress}) => {
           flexDirection: 'row',
           alignSelf: 'flex-start',
           alignItems: 'center',
-          justifyContent: 'center',
+          justifyContent: 'flex-start',
         }}>
         <TextKRBold
           style={{
@@ -26,6 +64,7 @@ const UserInfoTitle = ({userName, userAddress}) => {
             fontSize: 24,
             fontWeight: 'bold',
             lineHeight: 34,
+            paddingLeft: 3,
           }}>
           <View
             style={{
@@ -36,12 +75,13 @@ const UserInfoTitle = ({userName, userAddress}) => {
             }}>
             <TextKRBold
               style={{
-                padding: 3,
-                paddingBottom: 0,
+                paddingTop: 3,
+                textAlign: 'center',
+                textAlignVertical: 'center',
                 fontSize: 24,
                 color: PRIMARY_COLOR,
                 fontWeight: 'bold',
-                lineHeight: 30,
+                lineHeight: 34,
               }}>
               {userName}
             </TextKRBold>
@@ -51,15 +91,106 @@ const UserInfoTitle = ({userName, userAddress}) => {
       </View>
       <View
         style={{
+          marginVertical: 5,
           alignSelf: 'flex-start',
           justifyContent: 'center',
           alignItems: 'center',
-          padding: 5,
+          height: 24,
         }}>
-        <TextKRBold style={{color: WHITE_COLOR, fontWeight: '400'}}>
+        <TextKRBold
+          style={{
+            color: WHITE_COLOR,
+            fontWeight: '400',
+            fontSize: 14,
+            lineHeight: 24,
+          }}>
           배달 거점{'\t'}
-          <TextKRBold style={{fontWeight: 'bold'}}>{userAddress}</TextKRBold>
+          <TouchableOpacity
+            onPress={handleModal}
+            style={{
+              height: 20,
+              justifyContent: 'center',
+            }}>
+            <TextKRBold
+              style={{
+                fontSize: 14,
+                color: WHITE_COLOR,
+                fontWeight: 'bold',
+              }}>
+              서울과기대 {selectedAddress}
+            </TextKRBold>
+          </TouchableOpacity>
         </TextKRBold>
+        {/* <ChangeDormitory userAddress={userAddress} /> */}
+        <View>
+          <Modal
+            transparent={true}
+            visible={modal}
+            animationType={'slide'}
+            onRequestClose={handleModal}>
+            <View
+              style={{
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'rgba(0,0,0,0.45)',
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <View
+                style={{
+                  position: 'absolute',
+                  bottom: 0,
+                  width: '100%',
+                  height: 383,
+                  backgroundColor: WHITE_COLOR,
+                  borderTopLeftRadius: 10,
+                  borderTopRightRadius: 10,
+                }}>
+                <View
+                  style={{
+                    width: '100%',
+                    height: 35,
+                    justifyContent: 'center',
+                    borderBottomWidth: 1,
+                    borderBottomColor: LINE_GRAY_COLOR,
+                  }}>
+                  <TextKRReg
+                    style={{
+                      textAlign: 'center',
+                      fontWeight: '400',
+                      fontSize: 14,
+                      lineHeight: 24,
+                    }}>
+                    배달거점
+                  </TextKRReg>
+                </View>
+                <View>
+                  <FlatList data={DormitoryList} renderItem={renderItem} />
+                </View>
+                <TouchableOpacity
+                  onPress={handleModal}
+                  style={{
+                    width: '100%',
+                    justifyContent: 'center',
+                  }}>
+                  <TextKRBold
+                    style={{
+                      textAlign: 'center',
+                      paddingTop: 11,
+                      fontWeight: '400',
+                      fontSize: 16,
+                      lineHeight: 22,
+                    }}>
+                    확인
+                  </TextKRBold>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
+        </View>
+
         {/* 해당 부분 화살표가 선택지인지 혹은 그냥 디자인인지 확인 필요 */}
       </View>
     </>
