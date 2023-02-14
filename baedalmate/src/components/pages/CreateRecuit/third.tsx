203ea216886af3a461855426b2f6e08093c06498
byein <kyebin0825@gmail.com>
2023-02-14 23:14:51 +0900

    feat: add keyboardavoidview

--- baedalmate/src/components/pages/CreateRecuit/third.tsx
+++ baedalmate/src/components/pages/CreateRecuit/third.tsx
@@ -1,5 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import {ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 import {TextKRBold} from 'themes/text';
 import {Fonts} from 'assets/Fonts';
 import {
@@ -74,124 +82,129 @@ const CreateRecruit3 = props => {
       style={{
         backgroundColor: WHITE_COLOR,
       }}>
-      <ScrollView
-        style={{
-          backgroundColor: WHITE_COLOR,
-          marginBottom: 150,
-        }}>
-        <View style={{}}>
-          <View
-            style={{
-              padding: 15,
-              display: 'flex',
-            }}>
-            <TextKRBold style={styles.Title}>주문글 쓰기</TextKRBold>
-          </View>
-          <View
-            style={{
-              padding: 15,
-              backgroundColor: '#F7F8FA',
-              borderBottomWidth: 5,
-              borderBottomColor: WHITE_COLOR,
-            }}>
-            <View style={{flexDirection: 'row'}}>
-              <TextKRBold style={styles.Label}>제목</TextKRBold>
-              {errors.title && (
-                <Text style={styles.Validation}>제목을 적어주세요</Text>
-              )}
-            </View>
-
-            <TitleInput
-              error={errors}
-              name={'title'}
-              control={control}
-              rules={{required: true}}
-            />
-          </View>
-          <View
-            style={{
-              padding: 15,
-              display: 'flex',
-              flexDirection: 'column',
-              backgroundColor: '#F7F8FA',
-              borderBottomWidth: 5,
-              borderBottomColor: WHITE_COLOR,
-            }}>
-            <View style={{flexDirection: 'row'}}>
-              <TextKRBold style={styles.Label}>모집글 설명</TextKRBold>
-              {errors.title && (
-                <Text style={styles.Validation}>설명을 적어주세요</Text>
-              )}
-            </View>
-            <DescriptionInput
-              error={errors}
-              name={'description'}
-              control={control}
-              rules={{required: true}}
-            />
-          </View>
-          <View
-            style={{
-              padding: 15,
-              display: 'flex',
-              backgroundColor: '#F7F8FA',
-              borderBottomWidth: 5,
-              borderBottomColor: WHITE_COLOR,
-            }}>
-            <TextKRBold style={styles.Label}>태그쓰기</TextKRBold>
+      <KeyboardAvoidingView
+        // style={styles.avoidingView}
+        behavior={Platform.select({ios: 'padding'})}
+        keyboardVerticalOffset={44}>
+        <ScrollView
+          style={{
+            backgroundColor: WHITE_COLOR,
+            marginBottom: 150,
+          }}>
+          <View style={{}}>
             <View
               style={{
-                flexDirection: 'row',
-                justifyContent: 'space-between',
+                padding: 15,
+                display: 'flex',
               }}>
-              <TextInput
-                style={{
-                  backgroundColor: WHITE_COLOR,
-                  width: '70%',
-                  height: 44,
-                  borderRadius: 10,
-                  padding: 15,
-                }}
-                value={newTag}
-                onChangeText={text => {
-                  setNewTag(text);
-                }}
-                placeholder="#태그를 입력해주세요"></TextInput>
-              <BtnTag
-                text={'태그입력'}
-                onPress={() => {
-                  if (newTag !== '') {
-                    if (tagList.length < 4) {
-                      if (newTag.length <= 8) {
-                        setTagList([...tagList, {tagname: newTag}]);
-                        setNewTag('');
-                      }
-                    }
-                  }
-                }}
+              <TextKRBold style={styles.Title}>주문글 쓰기</TextKRBold>
+            </View>
+            <View
+              style={{
+                padding: 15,
+                backgroundColor: '#F7F8FA',
+                borderBottomWidth: 5,
+                borderBottomColor: WHITE_COLOR,
+              }}>
+              <View style={{flexDirection: 'row'}}>
+                <TextKRBold style={styles.Label}>제목</TextKRBold>
+                {errors.title && (
+                  <Text style={styles.Validation}>제목을 적어주세요</Text>
+                )}
+              </View>
+
+              <TitleInput
+                error={errors}
+                name={'title'}
+                control={control}
+                rules={{required: true}}
               />
             </View>
-            <View style={{flexDirection: 'row', marginVertical: 15}}>
-              {tagList.map((data, index) => (
-                <RecruitTag text={data.tagname} key={index} />
-              ))}
+            <View
+              style={{
+                padding: 15,
+                display: 'flex',
+                flexDirection: 'column',
+                backgroundColor: '#F7F8FA',
+                borderBottomWidth: 5,
+                borderBottomColor: WHITE_COLOR,
+              }}>
+              <View style={{flexDirection: 'row'}}>
+                <TextKRBold style={styles.Label}>모집글 설명</TextKRBold>
+                {errors.title && (
+                  <Text style={styles.Validation}>설명을 적어주세요</Text>
+                )}
+              </View>
+              <DescriptionInput
+                error={errors}
+                name={'description'}
+                control={control}
+                rules={{required: true}}
+              />
+            </View>
+            <View
+              style={{
+                padding: 15,
+                display: 'flex',
+                backgroundColor: '#F7F8FA',
+                borderBottomWidth: 5,
+                borderBottomColor: WHITE_COLOR,
+              }}>
+              <TextKRBold style={styles.Label}>태그쓰기</TextKRBold>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                }}>
+                <TextInput
+                  style={{
+                    backgroundColor: WHITE_COLOR,
+                    width: '70%',
+                    height: 44,
+                    borderRadius: 10,
+                    padding: 15,
+                  }}
+                  value={newTag}
+                  onChangeText={text => {
+                    setNewTag(text);
+                  }}
+                  placeholder="#태그를 입력해주세요"></TextInput>
+                <BtnTag
+                  text={'태그입력'}
+                  onPress={() => {
+                    if (newTag !== '') {
+                      if (tagList.length < 4) {
+                        if (newTag.length <= 8) {
+                          setTagList([...tagList, {tagname: newTag}]);
+                          setNewTag('');
+                        }
+                      }
+                    }
+                  }}
+                />
+              </View>
+              <View style={{flexDirection: 'row', marginVertical: 15}}>
+                {tagList.map((data, index) => (
+                  <RecruitTag text={data.tagname} key={index} />
+                ))}
+              </View>
             </View>
           </View>
+        </ScrollView>
+        <View
+          style={{
+            marginHorizontal: 15,
+          }}>
+          <View>
+            <View></View>
+            <BtnCreateFloating
+              onPress={handleSubmit(onSubmit)}
+              text={'다음으로'}
+              id={3}
+            />
+          </View>
         </View>
-      </ScrollView>
-      <View
-        style={{
-          marginHorizontal: 15,
-        }}>
-        <View>
-          <View></View>
-          <BtnCreateFloating
-            onPress={handleSubmit(onSubmit)}
-            text={'다음으로'}
-            id={3}
-          />
-        </View>
-      </View>
+      </KeyboardAvoidingView>
     </View>
   );
 };
